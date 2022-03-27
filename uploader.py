f4908912187e6c106737d795850f22edc17dc4b9
Cesare De Cal <edoardo.decal@gmail.com>
2022-03-27 18:57:46 +0200

    uploaded files

---
+++ uploader.py
@@ -0,0 +1,29 @@
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+import json
+import pygeohash as pgh
+
+
+# Setup Firebase
+cred = credentials.Certificate(
+    'certificate.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+print("Firebase Completed!")
+
+# Setup file
+with open('data-sample.json') as f:
+    data = json.load(f)
+print("File opened!")
+
+
+for location in data:
+    counter += 1
+    doc_ref = db.collection('elements').document()
+    loc = [location['l'][0], location['l'][1]]
+    doc_ref.set({
+        "g": location['g'],
+        "l": loc
+    })
+    print('Element uploaded')
